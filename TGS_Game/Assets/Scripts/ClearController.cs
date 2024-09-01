071ef23972a435537784fd51189a70973be8e0c8
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-09-01 22:13:01 +0900

    シーン遷移完成

---
+++ TGS_Game/Assets/Scripts/ClearController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClearController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ClearGame);
+    }
+
+
+    void ClearGame()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
